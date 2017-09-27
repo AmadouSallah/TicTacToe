df77e954fdf537abc4c65b9772d1bb1e635f2413
AmadouSallah <amadousallah13@gmail.com>
2017-09-27 12:38:44 -0700

    Added methods needed

---
+++ TicTacToe.java
@@ -0,0 +1,56 @@
+public class TicTacToe {
+
+  private char[][] board;
+  private char currentPlayer;
+
+  public TicTacToe() {
+    initializeBoard();
+    choosePlayer();
+  }
+
+  // make all slots of the board empty
+  private void initializeBoard() {
+
+  }
+
+  // prints the current board
+  private void printBoard() {
+
+  }
+
+  // checks whether the board is full or not. Returns true if board is full, and false otherwise
+  private boolean isBoardFull() {
+
+  }
+
+  // returns true if palyer is a winner
+  private boolean isWinner() {
+
+  }
+
+  // checks the rows for a win. Returns true if a win is found
+  private boolean checkRows() {
+
+  }
+
+  // checks the columns for a win. Returns true if a win is found
+  private boolean checkColumns() {
+
+  }
+
+  // checks the diagonals for a win. Returns true if a win is found
+  private boolean checkDiagonals() {
+
+  }
+
+  // checks if c1, c2, and c3 are similar, i.e 'X' or 'O'. If so, returns true
+  private boolean checkRowCol(char c1, char c2, char c3) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
+
+// Resources: https://medium.com/@ojusmilindsave/tutorial-to-implement-tic-tac-toe-in-java-ad639661a9b
